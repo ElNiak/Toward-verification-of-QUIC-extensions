238b7dcc7eaf93313181cb7664fe624b9654c03e
ElNiak <christophe.crochet@student.uclouvain.be>
2020-07-11 16:09:01 +0200

    change and move script

--- ivy/execute_file.sh
+++ ivy/execute_file.sh
@@ -5,22 +5,27 @@ if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
     echo "Illegal number of parameters"
 fi
 
-file_ivy=$1                 #e.g helloworld.ivy
-echo $file_ivy
+folder_file=$1
+file_ivy=$(basename $folder_file)   
+folder=$(dirname $folder_file)           
+echo "\nFolder: ${folder_file}"
+echo "File  : ${file_ivy}"
 file=$(echo "$file_ivy" | cut -f 1 -d '.')
-echo $file
+cd $folder
 
+echo "\n Mode:"
 if [ "$#" -eq 1 ]; then
-    echo "ivy_to_cpp target=repl"
+    echo "  ivy_to_cpp target=repl\n"
     ivy_to_cpp target=repl $file_ivy
 fi
 
 if [ "$#" -eq 2 ]; then
-    echo "ivy_to_cpp target=repl isolate=iso_impl"
+    echo "  ivy_to_cpp target=repl isolate=iso_impl\n"
     ivy_to_cpp target=repl isolate=iso_impl $file_ivy
 fi
 
 all_file="${file}.*"
+echo 'Files generated:'
 ls $all_file
 g++ -o $file "${file}.cpp" -lpthread
 ./$file
