83c7935ac858525be8fc8a366cdc535b0659d321
ElNiak <christophe.crochet@student.uclouvain.be>
2020-07-14 14:36:01 +0200

    removal of picoquic -> TODO: make install.sh that create automaticallya ll the folder

--- picoquic/picoquic/CMakeFiles/clangformat.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/clangformat"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/clangformat.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- picoquic/picoquic/CMakeFiles/picoquic-log.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libpicoquic-log.a"
-)
