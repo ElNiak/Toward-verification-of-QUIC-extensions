b58905109d5e87e132269bce28e63201273b9d80
chris <christophe.crochet@student.uclouvain.be>
2020-08-22 17:05:24 +0200

    nex doc

--- installer/TVOQE_18/installChromium.sh
+++ installer/TVOQE_18/installChromium.sh
@@ -1,17 +1,17 @@
-cd /home/chris/TVOQE_18/quic/
+cd /home/chris/TVOQE_18/quic/ #TODO
 
 #Install depot_tools
-[ ! -f /home/chris/TVOQE_18/quic/depot_tools ] && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH="$PATH:/home/chris/TVOQE_18/quic/depot_tools"
+[ ! -f depot_tools/ ] && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+export PATH="$PATH:/home/chris/TVOQE_18/quic/depot_tools" #TODO
 echo export PATH="$PATH:/home/chris/TVOQE_18/quic/depot_tools" >> ~/.profile 
 
 #Install chromium
 mkdir chromium
 cd chromium
-[ ! -f /home/chris/TVOQE_18/quic/chromium/src ] && fetch --nohooks chromium
+[ ! -f src/ ] && fetch --nohooks chromium
 cd src
-[ ! -f /home/chris/TVOQE_18/quic/chromium/src ] && ./build/install-build-deps.sh
-[ ! -f /home/chris/TVOQE_18/quic/chromium/src ] && gclient runhooks
-[ ! -f /home/chris/TVOQE_18/quic/chromium/src/out/Default ] && gn gen out/Default
+[ ! -f src/ ] && ./build/install-build-deps.sh
+[ ! -f src/ ] && gclient runhooks
+[ ! -f src/out/Default ] && gn gen out/Default
 #autoninja -C out/Default chrome
 ninja -C out/Default quic_server quic_client
