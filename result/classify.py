f781d6e04de29b31e0db71be18567a8150f0a8eb
chris <christophe.crochet@student.uclouvain.be>
2020-09-02 15:49:02 +0200

    reclassify result in folder pass vs fail

--- result/classify.py
+++ result/classify.py
@@ -6,59 +6,54 @@ import shutil
 
 #https://stackoverflow.com/questions/3346430/what-is-the-most-efficient-way-to-get-first-and-last-line-of-a-text-file/3346788
 def readlastline(f):
-    f.seek(-2, 2)              # Jump to the second last byte.
-    while f.read(1) != b"\n":  # Until EOL is found ...
-        f.seek(-2, 1)          # ... jump back, over the read byte plus one more.
-    return f.read()            # Read all data from this point on.
+    for line in f:
+    	pass
+    return line
 
 
-foldername = sys.argv[1]
+
+foldername =  sys.argv[1] #"/home/chris/Toward-verification-of-QUIC-extensions/result/"+
 
 try:
     os.mkdir(foldername+"/pass")
     os.mkdir(foldername + "/fail")
 except OSError:
-    print ("Creation of the directory %s failed" % path)
+    print ("Creation of the directory %s failed" % foldername)
 else:
-    print ("Successfully created the directory %s " % path)
+    print ("Successfully created the directory %s " % foldername)
 
 # TODO make more clean
 for file in os.listdir(foldername):
     if file.endswith(".iev"):
         fullPath = os.path.join(foldername, file)
-        out = os.path.join(foldername, file.replace(".iev",".out"))
-        err = os.path.join(foldername, file.replace(".iev", ".err"))
+        out = file.replace(".iev",".out")
+	outPath = os.path.join(foldername, out)
+        err = file.replace(".iev", ".err")
+	errPath = os.path.join(foldername, err)
         with open(fullPath, "r") as f:
             last = readlastline(f)
-            if last == "test_completed":
+            if last in "test_completed\n":
                 newFullPathPass = os.path.join(foldername + "/pass", file)
                 fullOutPass = os.path.join(foldername + "/pass", out)
                 fullErrPass = os.path.join(foldername + "/pass", err)
-                os.rename(fullPath, newFullPathPass)
-                shutil.move(fullPath,newFullPathPass)
-                os.replace(fullPath, newFullPathPass)
 
-                os.rename(out, fullOutPass)
-                shutil.move(out, fullOutPass)
-                os.replace(out, fullOutPass)
+                if(os.path.exists(fullPath)): 
+			shutil.move(fullPath,newFullPathPass)
+                if(os.path.exists(outPath)): 
+			shutil.move(outPath, fullOutPass)
+                if(os.path.exists(errPath)): 
+			shutil.move(errPath, fullErrPass)
 
-                os.rename(err, fullErrPass)
-                shutil.move(err, fullErrPass)
-                os.replace(err, fullErrPass)
             else:
                 newFullPathFail = os.path.join(foldername + "/fail", file)
                 fullOutFail = os.path.join(foldername + "/fail", out)
                 fullErrFail = os.path.join(foldername + "/fail", err)
 
-                os.rename(fullPath, newFullPathFail)
-                shutil.move(fullPath,newFullPathFail)
-                os.replace(fullPath, newFullPathFail)
+                if(os.path.exists(fullPath)): 
+			 shutil.move(fullPath,newFullPathFail)
+                if(os.path.exists(outPath)): 
+			 shutil.move(outPath, fullOutFail)
+                if(os.path.exists(errPath)): 
+			 shutil.move(errPath, fullErrFail)
 
-                os.rename(out, fullOutFail)
-                shutil.move(out, fullOutFail)
-                os.replace(out, fullOutFail)
-
-                os.rename(err, fullErrFail)
-                shutil.move(err, fullErrFail)
-                os.replace(err, fullErrFail)
 
