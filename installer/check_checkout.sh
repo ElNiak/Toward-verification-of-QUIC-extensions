3ae372abedac26def46b604b14e6874beb29cfa9
ElNiak <#Clubdes3c>
2020-11-14 18:13:14 +0100

    commit checker

--- installer/check_checkout.sh
+++ installer/check_checkout.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-git log --pretty=format:"%H"
+IFS=$'\n'
+commits=( $(git log --pretty=format:"%H") )
+declare -p commits
+
+# We are (THE CHAMPION) in quic folder
+# ${1} = quic version
+for commit in "${commits[@]}"
+do
+    git checkout commit
+    ivyc target=test quic_server_test_stream.ivy
+    python test/test.py iters=1 server=picoquic test=quic_server_test_stream stats=true >> ${1}_test_checkout.txt
+done
