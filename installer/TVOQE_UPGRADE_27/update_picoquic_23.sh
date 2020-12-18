a4f5ca822abeb1e77fb0c1d99e24af7c7918d349
ElNiak <christophe.crochet@student.uclouvain.be>
2020-12-18 15:29:21 +0100

    add updater

--- installer/TVOQE_UPGRADE_27/update_picoquic_23.sh
+++ installer/TVOQE_UPGRADE_27/update_picoquic_23.sh
@@ -2,7 +2,7 @@
 printf "\n\n"
 printf "###### Installing PicoTLS:\n\n"
 cd $HOME/TVOQE_UPGRADE_27/quic/picotls/
-git checkout 9e6b595dd548f1662483704d4efffbe01696b311 #3fdf6a54c4c0762226afcbabda3b2016af5a8761
+git checkout 3fdf6a54c4c0762226afcbabda3b2016af5a8761
 git submodule init
 git submodule update
 cmake .
@@ -14,7 +14,7 @@ make check
 printf "\n\n"
 printf "###### Installing PicoQUIC:\n\n"
 cd $HOME/TVOQE_UPGRADE_27/quic/picoquic/
-git checkout c8e15c92ae5b76604ae176ba14772c1685ad8aca 
+git checkout 236a754f32860f58af9b129b62f6836c76c8a748 #c8e15c92ae5b76604ae176ba14772c1685ad8aca 
 cmake .
 make
 ./picoquic_ct
