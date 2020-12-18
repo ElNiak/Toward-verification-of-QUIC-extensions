df1a56510106755724dc00919fcc7b287a6353f4
ElNiak <christophe.crochet@student.uclouvain.be>
2020-12-18 09:54:26 +0100

    add updater

--- installer/TVOQE_UPGRADE_27/install_ivy.sh
+++ installer/TVOQE_UPGRADE_27/install_ivy.sh
@@ -7,8 +7,8 @@ echo $array
 
 SUB='test'
 for j in "${array[@]}"; do : 
-    printf "Files => $j  \n" 
     if [[ ! "$j" == *"$SUB"* ]]; then
+        printf "Files => $j  \n" 
     	sudo cp $j /usr/local/lib/python2.7/dist-packages/ivy/include/1.7
     fi
 done
---
+++ installer/TVOQE_UPGRADE_27/update_picoquic_23.sh
@@ -0,0 +1,20 @@
+ #Install picotls
+printf "\n\n"
+printf "###### Installing PicoTLS:\n\n"
+cd $HOME/TVOQE_UPGRADE_27/quic/picotls/
+git checkout 3fdf6a54c4c0762226afcbabda3b2016af5a8761
+git submodule init
+git submodule update
+cmake .
+make
+make check
+
+
+#Install picoquic
+printf "\n\n"
+printf "###### Installing PicoQUIC:\n\n"
+cd $HOME/TVOQE_UPGRADE_27/quic/picoquic/
+git checkout b6f65b09c9acff333028413ade4b1229c7bac025 
+cmake .
+make
+./picoquic_ct
