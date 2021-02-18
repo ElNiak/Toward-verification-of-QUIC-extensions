c0265768fa95e4b50fd5f63803f6e8831a36c510
ElNiak <christophe.crochet@student.uclouvain.be>
2021-02-18 14:54:48 +0100

    update test launch

--- installer/TVOQE/res/install_quic_implementation.sh
+++ installer/TVOQE/res/install_quic_implementation.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 cd /quic
-[ ! -f picotls/ ] &&  git clone https://github.com/h2o/picotls.git
-[ ! -f picoquic/ ] &&  git clone https://github.com/private-octopus/picoquic.git 
 
 #Install picotls
 printf "\n\n"
 printf "###### Installing PicoTLS:\n\n"
 cd /quic/picotls/
-git checkout 2464adadf28c1b924416831d24ca62380936a209
-git submodule init
-git submodule update
 cmake .
 make
 make check
@@ -19,7 +14,6 @@ make check
 printf "\n\n"
 printf "###### Installing PicoQUIC:\n\n"
 cd /quic/picoquic/
-git checkout 639c9e685d37e74d357d3dd8599b9dbff90934af 
 cmake .
 make
 ./picoquic_ct
