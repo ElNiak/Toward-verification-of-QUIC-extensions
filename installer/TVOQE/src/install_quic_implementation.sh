948bb4ace191cd2ec0e36a828d25fa0a9175dd55
ElNiak <christophe.crochet@student.uclouvain.be>
2021-03-08 15:07:46 +0100

    update docker installer

--- installer/TVOQE/src/install_quic_implementation.sh
+++ installer/TVOQE/src/install_quic_implementation.sh
@@ -43,7 +43,7 @@ pip3 install aiofiles asgiref dnslib httpbin starlette wsproto
 #Install Quant
 cd /
 sudo apt remove --fix-missing -y cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.12.4-Linux-x86_64.sh  &> /dev/null
+wget https://github.com/Kitware/CMake/releases/download/v3.12.4/cmake-3.12.4-Linux-x86_64.sh  &> /dev/null
 # tar xfz cmake-3.19.6-Linux-x86_64.tar.gz  &> /dev/null
 # cd cmake-3.19.6-Linux-x86_64
 # ./configure
