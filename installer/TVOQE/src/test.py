78698b0fcf34f7b7c3814edad380dcd60de32e9d
ElNiak <christophe.crochet@student.uclouvain.be>
2021-03-09 10:33:56 +0100

    update docker installer

--- installer/TVOQE/src/test.py
+++ installer/TVOQE/src/test.py
@@ -247,7 +247,7 @@ class Test(object):
                 with open_out(self.name+str(test_command)+'.iev') as iev:
                     # If run => Launch the quic entity tested 
                     if run:
-                        qcmd = 'sleep 2; ' + quic_cmd if is_client else quic_cmd.split() 
+                        qcmd = 'sleep 1; ' + quic_cmd if is_client else quic_cmd.split() 
                         print 'implementation command: {}'.format(qcmd)
                         quic_process = subprocess.Popen(qcmd,
                                                   cwd=quic_dir,
