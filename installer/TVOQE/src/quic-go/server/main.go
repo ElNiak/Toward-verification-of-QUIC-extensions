d64b9e4a041073f561faf3073671b49f66bd63ab
ElNiak <christophe.crochet@student.uclouvain.be>
2021-03-10 14:22:11 +0100

    update docker installer

--- installer/TVOQE/src/quic-go/server/main.go
+++ installer/TVOQE/src/quic-go/server/main.go
@@ -26,13 +26,8 @@ func main() {
 	_logFile := flag.String("l", "/logs/log.txt", "private key file")
 	port := flag.Int("p", 443, "port to bind on")
 	flag.Parse()
-	logFile, err := os.Create(*_logFile)
-	if err != nil {
-		fmt.Printf("Could not create log file: %s\n", err.Error())
-		os.Exit(1)
-	}
-	defer logFile.Close()
-	log.SetOutput(logFile)
+
+	log.SetOutput(os.Stdout)
 
 	keyLog, err := utils.GetSSLKeyLog()
 	if err != nil {
