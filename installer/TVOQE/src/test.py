cd19c08f52a7494bfcf2ffdb89919b3da01dedfe
ElNiak <christophe.crochet@student.uclouvain.be>
2021-04-09 08:07:13 +0200

    [update] minor change

--- installer/TVOQE/src/test.py
+++ installer/TVOQE/src/test.py
@@ -333,7 +333,7 @@ class IvyTest(Test):
     def command(self,test_command):
         import platform
         timeout_cmd = '' if platform.system() == 'Windows' else 'timeout {} '.format(time)
-        randomSeed = random.Random()
+        randomSeed = random.random()
         random.seed(datetime.now())
         return ' '.join(['{}./build/{} seed={} the_cid={} {}'.format(timeout_cmd,self.name,randomSeed,2*test_command,'' if is_client else 'server_cid={} client_port={} client_port_alt={}'.format(2*test_command+1,2*test_command+4987,2*test_command+4988))] + extra_args)
 
