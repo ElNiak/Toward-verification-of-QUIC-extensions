61201019b13f1221197cccc7ace71d94610276a9
ElNiak <christophe.crochet@student.uclouvain.be>
2021-05-08 23:28:31 +0200

    [update] server launch test

--- installer/TVOQE/src/test_client.sh
+++ installer/TVOQE/src/test_client.sh
@@ -21,7 +21,7 @@ tests_client=(quic_client_test_max
               quic_client_test_tp_limit_acticoid_error
               quic_client_test_blocked_streams_maxstream_error
               #quic_client_test_retirecoid_error
-              q#uic_client_test_token_error
+              #quic_client_test_token_error
               quic_client_test_newcoid_zero_error
               quic_client_test_accept_maxdata
               quic_client_test_tp_prefadd_error
