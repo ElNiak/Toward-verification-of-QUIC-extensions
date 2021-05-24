9c584643845f421ae181798941a3194a59b5cd0f
ElNiak <christophe.crochet@student.uclouvain.be>
2021-05-24 15:24:56 +0200

    crypto server limit

--- installer/TVOQE/results/trace_to_tex.py
+++ installer/TVOQE/results/trace_to_tex.py
@@ -45,45 +45,48 @@ def write_header_tex(writer):
     writer.write('\hline\n')
     writer.write('\multicolumn{1}{|c|}{\\textbf{src}} & \multicolumn{1}{c|}{\\textbf{dst}} & \multicolumn{1}{c|}{\\textbf{time}} & \multicolumn{1}{}{} & \multicolumn{1}{c|}{\\textbf{Information}} \\\\ \hline \n')
 
+
 def write_footer_tex(writer):
     writer.write('\hline\n')
     writer.write('\end{tabular}%\n')
     writer.write('}\n')
     writer.write('\end{table}\n')
 
+
 def get_line(frames, last_pnum, pkt, pkt_len, pkt_type, dcid):
     if pkt_type == '0' and hasattr(pkt.quic, "long_packet_type"):
         pkt_t = "Initial"
         scid = pkt.quic.scid.replace(":", "")
         line = str(pkt_len) + ", " + pkt_t + ", DCID=" + str(dcid) + ", SCID=" + str(
-                                    scid) + ", PKT: " + str(last_pnum) + ", " + frames
+            scid) + ", PKT: " + str(last_pnum) + ", " + frames
     elif pkt_type == '0':
         pkt_t = "Protected Payload (KP0)"
         line = str(pkt_len) + ", " + pkt_t + ", DCID=" + str(dcid) + \
-                                    ", PKT: " + str(last_pnum) + ", " + frames
+            ", PKT: " + str(last_pnum) + ", " + frames
     elif pkt_type == '2':
         pkt_t = "Handshake"
         scid = pkt.quic.scid.replace(":", "")
         line = str(pkt_len) + ", " + pkt_t + ", DCID=" + str(dcid) + ", SCID=" + str(
-                                    scid) + ", PKT: " + str(last_pnum) + ", " + frames
+            scid) + ", PKT: " + str(last_pnum) + ", " + frames
     return line
+
 
 def write_line(f, dst_port, src_port, line):
     if src_port == "4443":
         f.write('\\rowcolor{LightCyan} ' + str(src_port) + " & " + str(
-                                    dst_port) + " &  & \multicolumn{1}{c}{} &")
+            dst_port) + " &  & \multicolumn{1}{c}{} &")
         f.write(line)
         f.write('\\\\ \n')
     else:
         f.write('\\rowcolor{lightgreen} ' + str(src_port) + " & " + str(
-                                    dst_port) + " &  & \multicolumn{1}{c}{} &")
+            dst_port) + " &  & \multicolumn{1}{c}{} &")
         f.write(line)
         f.write('\\\\ \n')
+
 
 foldername = "/home/chris/Toward-verification-of-QUIC-extensions/installer/TVOQE/results/pass/server/local/stream/"
 subfolders = [f.path for f in scandir.scandir(foldername) if f.is_dir()]
 run = 0
-
 
 
 for fol in subfolders:
@@ -127,48 +130,48 @@ for fol in subfolders:
             f = open(outputFile, "w")
             write_header_tex(f)
 
-
-            frames = ""  
+            frames = ""
             last_pnum = "0"
             first = True
 
             for pkt in packets:
-                for layer in pkt:
-                    if layer.layer_name == 'quic':
+                # for layer in pkt:
+                # if layer.layer_name == 'quic':
 
-                        dst_port = pkt.udp.dstport
-                        src_port = pkt.udp.srcport
+                dst_port = pkt.udp.dstport
+                src_port = pkt.udp.srcport
 
-                        pkt_len = pkt.quic.packet_length
-                        pkt_type = pkt.quic.header_form
-                        if hasattr(pkt.quic, "long_packet_type"):
-                            pkt_type = pkt.quic.long_packet_type
-                        
-                        dcid = pkt.quic.dcid.replace(":", "")
-                        pkt_t = ""
-                        pkt_num = pkt.quic.packet_number
-                        scid = None
-                        line = ""
+                pkt_len = pkt.quic.packet_length
+                pkt_type = pkt.quic.header_form
+                if hasattr(pkt.quic, "long_packet_type"):
+                    pkt_type = pkt.quic.long_packet_type
 
-                        if first:
-                            last_pnum = pkt.quic.packet_number
-                            fr = pkt.quic.frame
-                            frame = replace_tag(fr)
-                            frames = frames + frame + " "
-                            first = False
-                            # line = get_line(frames, last_pnum, pkt, pkt_len, pkt_type, dcid)
-                            # write_line(f, dst_port, src_port, line)   
-                        elif last_pnum == pkt_num:
-                            fr = pkt.quic.frame
-                            frame = replace_tag(fr)
-                            frames = frames + frame + " "
-                        else:
-                            line = get_line(frames, last_pnum, pkt, pkt_len, pkt_type, dcid)
-                            write_line(f, dst_port, src_port, line)
-                            last_pnum = pkt_num
-                            fr = pkt.quic.frame
-                            frame = replace_tag(fr)
-                            frames =  frame + " "
+                dcid = pkt.quic.dcid.replace(":", "")
+                pkt_t = ""
+                pkt_num = pkt.quic.packet_number
+                scid = None
+                line = ""
+
+                if first:
+                    last_pnum = pkt.quic.packet_number
+                    fr = pkt.quic.frame
+                    frame = replace_tag(fr)
+                    frames = frames + frame + " "
+                    first = False
+                    # line = get_line(frames, last_pnum, pkt, pkt_len, pkt_type, dcid)
+                    # write_line(f, dst_port, src_port, line)
+                elif last_pnum == pkt_num:
+                    fr = pkt.quic.frame
+                    frame = replace_tag(fr)
+                    frames = frames + frame + " "
+                else:
+                    line = get_line(frames, last_pnum, pkt,
+                                    pkt_len, pkt_type, dcid)
+                    write_line(f, dst_port, src_port, line)
+                    last_pnum = pkt_num
+                    fr = pkt.quic.frame
+                    frame = replace_tag(fr)
+                    frames = frame + " "
 
             write_footer_tex(f)
             f.close()
